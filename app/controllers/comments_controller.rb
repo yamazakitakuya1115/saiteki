c22b343cba86a18eaa985ce935b7707182ec77d2
yamazakitakuya <71809256+yamazakitakuya1115@users.noreply.github.com>
2020-11-06 22:13:09 +0900

    コメント機能修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,15 @@
 class CommentsController < ApplicationController
 
   def create
-    if Comment.create(comment_params)
-      redirect_to root_path
+    comment = Comment.new(comment_params)
+    if comment.save
+      redirect_to "/articles/#{comment.article.id}"
     end
   end
 
   private
 
   def comment_params
-    params.permit(:message).merge(article_id: params[:article_id].to_i, user_id: current_user.id)
+    params.require(:comment).permit(:message).merge(article_id: params[:article_id].to_i, user_id: current_user.id)
   end
 end
